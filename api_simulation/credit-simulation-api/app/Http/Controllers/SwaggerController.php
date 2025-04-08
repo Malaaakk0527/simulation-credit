137d5c4f5892814bcea27d2a74436434629290e0
malak <assilnourdo@gmail.com>
2025-04-08 17:07:27 +0200

    Ajout du projet React et Laravel

---
+++ api_simulation/credit-simulation-api/app/Http/Controllers/SwaggerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *     title="API Simulation Crédit",
+ *     version="1.0.0",
+ *     description="Documentation de l'API de simulation de crédit"
+ * )
+ *
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Serveur local"
+ * )
+ *
+ * @OA\PathItem(path="/")
+ */
+class SwaggerController
+{
+    //
+}
